506db81d7631ce8e60c5da15be915c19f7e69ac8
mdujjal67 <mdujjal.mm67@gmail.com>
2024-04-27 13:34:31 +0600

    data created for add spots page

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// tourismManager
-// XpBIgG4DyJJT7Hd4
 
 // ------------MongoDB Start----------
 
-const uri = "mongodb+srv://tourismManager:XpBIgG4DyJJT7Hd4@cluster0.7tyfnet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7tyfnet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +27,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const spotCollection = client.db('tourismDB').collection('/addSpots');
+
+    app.post('/addSpots', async(req, res) => {
+      const newAddedSpot = req.body
+      console.log(newAddedSpot)
+      const result = await spotCollection.insertOne(newAddedSpot)
+      res.send(result)
+
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
